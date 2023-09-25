17cf870d916906ffb68b219fac0b4e05a36c09aa
Mohamad Safa <mohsafa3@gmail.com>
2023-09-25 16:02:32 +0300

    Header editing

--- frontend/portfolio/src/App.js
+++ frontend/portfolio/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import PortfolioDashboard from './PortfolioDashboard';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PortfolioDashboard from "./PortfolioDashboard";
+import Header from "./components/header";
+import TextImageSection from "./components/image-header";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<PortfolioDashboard />} />
-       
-      </Routes>
-    </Router>
+    <div>
+      <Header/>
+      <TextImageSection/>
+    </div>
   );
 }
 
